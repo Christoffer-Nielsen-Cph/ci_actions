eb62489cbc0625dec042d7c34a1ea11956c1bb4c
Chris <cph-cn332@cphbusiness.dk>
2022-08-26 09:21:56 +0200

    mysql added

--- src/main/java/ci/UserMapper.java
+++ src/main/java/ci/UserMapper.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserMapper {
+public class    UserMapper {
 
     Connection con = null;
 
